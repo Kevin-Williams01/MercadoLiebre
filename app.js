0c817bb1a123e09b8e9faaa8e2718ec9a1f46c25
Rascasse01 <k12williams@hotmail.com>
2023-05-08 20:24:00 -0300

    Version actualizada

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ app.get('/', (req, res) => {
     res.sendFile(htmlPath);
 });
 
+app.get('/register', (req, res) => {
+    const htmlPath = path.join(__dirname, '/views/register.html');
+
+    res.sendFile(htmlPath);
+});
+
+app.get('/login', (req, res) => {
+    const htmlPath = path.join(__dirname, '/views/login.html');
+
+    res.sendFile(htmlPath);
+});
+
 app.listen(3000, (req, res) => {
     console.log('Escuchando en el puerto 3000');
 });
